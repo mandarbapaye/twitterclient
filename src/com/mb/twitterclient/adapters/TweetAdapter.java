0f5b36afb27ca60b04a2546af2c803681dfc4ca1
Mandar Bapaye <mandarbapaye@yahoo.com>
2014-06-20 02:47:59 -0700

    ago time calculation

--- src/com/mb/twitterclient/adapters/TweetAdapter.java
+++ src/com/mb/twitterclient/adapters/TweetAdapter.java
@@ -1,8 +1,14 @@
 package com.mb.twitterclient.adapters;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,6 +42,7 @@ public class TweetAdapter extends ArrayAdapter<Tweet> {
 		TextView tvName = (TextView) v.findViewById(R.id.tvName);
 		TextView tvScreenName = (TextView) v.findViewById(R.id.tvScreenName);
 		TextView tvTweet = (TextView) v.findViewById(R.id.tvTweet);
+		TextView tvTimeAgo = (TextView) v.findViewById(R.id.tvTimeAgo);
 
 		ivProfileImg.setImageResource(android.R.color.transparent);
 		ImageLoader imageLoader = ImageLoader.getInstance();
@@ -43,12 +50,41 @@ public class TweetAdapter extends ArrayAdapter<Tweet> {
 		tvTweet.setText(tweet.getBody());
 		tvName.setText(tweet.getUser().getName());
 		tvScreenName.setText("@" + tweet.getUser().getScreenName());
+		tvTimeAgo.setText(getRelativeTimeAgo(tweet.getCreatedAt()));
 
 		return v;
 	}
 
+	
+	public String getRelativeTimeAgo(String rawJsonDate) {
+		String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+		SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+		sf.setLenient(true);
+	 
+		long msFor7Days = 604800000; 
+		try {
+			long dateMillis = sf.parse(rawJsonDate).getTime();
+			long agoMs = System.currentTimeMillis() - dateMillis;
+			if (agoMs > (msFor7Days - 1)) {
+				SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM", Locale.ENGLISH);
+				return dateFormat.format(new Date(dateMillis));
+			} else {
+				String relativeDateFull = DateUtils.getRelativeTimeSpanString(dateMillis,
+										 System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+				Scanner scanner = new Scanner(relativeDateFull);
+				scanner.useDelimiter(" ");
+				
+				StringBuilder relativeDateShort = new StringBuilder();				
+				relativeDateShort.append(scanner.next());
+				relativeDateShort.append(scanner.next().charAt(0));
+				return relativeDateShort.toString();
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return "";
+	}
 
 	
-	
-
 }
