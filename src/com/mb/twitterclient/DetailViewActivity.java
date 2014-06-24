9f0fbf22c584a3b4d76638939cd8d81aa4b7b3de
Mandar Bapaye <mandarbapaye@yahoo.com>
2014-06-24 00:21:57 -0700

    UP button in detail view

--- src/com/mb/twitterclient/DetailViewActivity.java
+++ src/com/mb/twitterclient/DetailViewActivity.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.text.format.DateUtils;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,6 +31,7 @@ public class DetailViewActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail_view);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
 		ivDetailImage = (ImageView) findViewById(R.id.ivDetailImage);
 		tvDetailName = (TextView) findViewById(R.id.tvDetailName);
@@ -41,6 +44,17 @@ public class DetailViewActivity extends Activity {
 		if (tweet != null) {
 			showTweetDetails(tweet);
 		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			// Respond to the action bar's Up/Home button
+	    	case android.R.id.home:
+	    		NavUtils.navigateUpFromSameTask(this);
+	    		return true;
+	    }
+	    return super.onOptionsItemSelected(item);
 	}
 
 	private void showTweetDetails(Tweet tweet) {
